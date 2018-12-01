231ab5b8e8cf32c79ce60e816d1967356e8e4917
League <league@WTS9.attlocal.net>
2018-12-01 15:55:20 -0800

    Jukebox

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -24,6 +24,7 @@ public class MagicBox extends JPanel implements Runnable, MouseListener {
 	 * When the user clicks on a secret place, stuff will happen.
 	 * 
 	 * 1. Make the frame respond to mouse clicks.
+	 
 	 * 
 	 * 2. When the mouse is clicked, use the Media Palace (read the code in the default package) to play sounds, show images or speak.
 	 * 
@@ -56,6 +57,7 @@ public class MagicBox extends JPanel implements Runnable, MouseListener {
 		frame.pack();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
+		frame.addMouseListener(this);
 	}
 
 	private void loadBackgroundImage() throws Exception {
